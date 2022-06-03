25a92fe0733676a8fd16f0338aedf0ee1bdb4e3d
newlandslucas <lucasnewlands.m@gmail.com>
2022-06-03 15:13:52 -0300

    Initial Commit

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Lucas Machado (Estudos) on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
